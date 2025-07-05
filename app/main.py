3310f29467f46cdba0e928e5c79905594195ab27
P.V.S.1905 <viktor66462@gmail.com>
2025-07-05 03:18:17 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -2,16 +2,19 @@ from app.models import Book, BookDisplay, BookPrint, BookSerializer
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
-    book_displays = BookDisplay(book)
-    book_print = BookPrint(book)
-    book_serialize = BookSerializer(book)
-    for cmd, method_type in commands:
-        if cmd == "display":
-            book_displays.display(method_type)
-        elif cmd == "print":
-            book_print.print_book(method_type)
-        elif cmd == "serialize":
-            return book_serialize.serialize(method_type)
+    try:
+        book_displays = BookDisplay(book)
+        book_print = BookPrint(book)
+        book_serialize = BookSerializer(book)
+        for cmd, method_type in commands:
+            if cmd == "display":
+                book_displays.display(method_type)
+            elif cmd == "print":
+                book_print.print_book(method_type)
+            elif cmd == "serialize":
+                return book_serialize.serialize(method_type)
+    except Exception as e:
+        print(f"Exception: {e}")
 
 
 if __name__ == "__main__":
